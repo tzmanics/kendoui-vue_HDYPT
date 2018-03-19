1712ea46b2910801bbdf9d9756e2c278b379e971
tzmanics <tzmanics@gmail.com>
2018-03-19 01:30:09 -0400

    initial commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import {
+  Button,
+  ButtonsInstaller
+} from '@progress/kendo-buttons-vue-wrapper'
+
+Vue.config.productionTip = false
+Vue.use(ButtonsInstaller)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: {
+    App,
+    Button
+  }
+})
